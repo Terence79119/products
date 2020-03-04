eb3df6e883a347150c20e6d807b8df24d9ef4d00
Terence Wang <terence79119@gmail.com>
2020-03-04 18:14:53 +0800

    file check function

--- products/products.py
+++ products/products.py
@@ -1,11 +1,18 @@
-#讀取檔案
+import os #operating system
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
+#檢查檔案在不在，讀取檔案
+if os.path.isfile('products.csv'): 
+	print('yeah! founded')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+else:
+	print('no file......')
+	
+
 #讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
